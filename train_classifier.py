5c0a79921a96716cc989bab697b7964de0e6e115
PriyanshiVerma10 <152619034+PriyanshiVerma10@users.noreply.github.com>
2025-08-02 17:58:07 +0530

    Add files via upload

--- train_classifier.py
+++ train_classifier.py
@@ -1,5 +1,7 @@
 import pickle
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -13,10 +15,16 @@ labels = np.asarray(data_dict['labels'])
 
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-model = RandomForestClassifier()
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
 
 model.fit(x_train, y_train)
-
 y_predict = model.predict(x_test)
 
 score = accuracy_score(y_predict, y_test)
